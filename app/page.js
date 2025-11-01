28daf18feced520b29165da0b0fca0054d907b4a
j-jh <greenjello106@gmail.com>
2025-11-01 16:23:57 -0700

    feat: static home page component with neighborhood and street count input

--- app/page.js
+++ app/page.js
@@ -1,65 +1,52 @@
-import Image from "next/image";
-
+"use client"
+// Home screen
+import { useState } from "react";
+/*
+  Render all unique neighborhoods as drop down menu for user to select
+*/
 export default function Home() {
+
+  const [streetCount, setStreetCount] = useState("");
+  const [selectedNeighborhood, setSelectedNeighborhood] = useState("");
+  const fakeNeighborhoods = ["Cool neighborhood", "Uncool neighborhood", "Decent place", "Newtown", "Oldtown"]
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("boo!");
+    const defaultCount = streetCount || 10;
+    setStreetCount(defaultCount);
+    console.log("Selected: " + selectedNeighborhood);
+    console.log("Street count: " + defaultCount);
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div>
+      <h1>OnlyTreats</h1>
+      <form onSubmit={handleSubmit}>
+        <select
+          value={selectedNeighborhood}
+          onChange={(e) => {
+            setSelectedNeighborhood(e.target.value);
+            e.target.setCustomValidity("");
+          }
+          }
+          required
+          onInvalid={(e) => e.target.setCustomValidity("Select an option")}
+        >
+          <option value="" disabled>Select a neighborhood</option>
+          {fakeNeighborhoods.map(item => (
+            <option key={item} value={item}>{item}</option>
+          ))}
+        </select>
+        <input
+          placeholder="Number of streets"
+          value={streetCount}
+          type="number"
+          onChange={(e) => setStreetCount(e.target.value)}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        <button type="submit">Spooky Search</button>
+      </form>
     </div>
   );
 }
