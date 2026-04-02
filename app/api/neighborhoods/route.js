dee7f763c5d0aebb8957309d69b011dd938d2c74
j-jh <greenjello106@gmail.com>
2026-04-01 23:06:53 -0700

    refactor: return hardcoded list of neighborhoods for faster response

--- app/api/neighborhoods/route.js
+++ app/api/neighborhoods/route.js
@@ -2,51 +2,118 @@
     route: /api/neighborhoods 
     ---
     GET API endpoint
-    Fetch and sort list of all San Francisco neighborhoods from data.sfgov.org API 
+    Return hardcoded list of all San Francisco neighborhoods
     ---
     Headers:
-        Content-Type: application
+        Content-Type: application/json
     HTTP Query Params: null
     Function Params: null
         
     Behaviors:
-    - Fetch neighborhood JSON data from data.sfgov.org API
-    - Return error on API failure
-    - Sort neighborhood names by alphabetical order
+    - Return hardcoded list of SF neighborhoods (sorted alphabetically)
+    - Instant response, no external API calls
 
     Response JSON: 
-    - Return list of sorted neighborhoods on success
-
-    Errors:
-    - 500: Failure on data.sfgov.org API
+    - Return list of neighborhoods on success
 */
-export async function GET() {
-    try {
-        const sfDataResults = await fetch('https://data.sfgov.org/resource/dx7g-zwbx.json?$select=neighborhood&$order=neighborhood');
+export function GET() {
+    const neighborhoods = [
+        "Alamo Square",
+        "Anza Vista",
+        "Balboa Terrace",
+        "Bayview",
+        "Bayview Heights",
+        "Bernal Heights",
+        "Bernal Heights South",
+        "Buena Vista Park/Ashbury Heights",
+        "Candlestick Point",
+        "Central Richmond",
+        "Central Sunset",
+        "Central Waterfront/Dogpatch",
+        "Clarendon Heights",
+        "Cole Valley/Parnassus Heights",
+        "Corona Heights",
+        "Cow Hollow",
+        "Croker Amazon",
+        "Diamond Heights",
+        "Downtown",
+        "Duboce Triangle",
+        "Eureka Valley/Dolores Heights",
+        "Excelsior",
+        "Financial District North",
+        "Financial District South",
+        "Forest Hill",
+        "Forest Hill Extension",
+        "Forest Knolls",
+        "Glen Park",
+        "Golden Gate Heights",
+        "Haight Ashbury",
+        "Hayes Valley",
+        "Hunters Point",
+        "Ingleside",
+        "Ingleside Heights",
+        "Ingleside Terrace",
+        "Inner Mission",
+        "Inner Parkside",
+        "Inner Richmond",
+        "Inner Sunset",
+        "Jordan Park/Laurel Heights",
+        "Lake Shore",
+        "Lake Street",
+        "Lakeside",
+        "Little Hollywood",
+        "Lone Mountain",
+        "Lower Pacific Heights",
+        "Marina",
+        "Merced Heights",
+        "Merced Manor",
+        "Midtown Terrace",
+        "Miraloma Park",
+        "Mission Bay",
+        "Mission Dolores",
+        "Mission Terrace",
+        "Monterey Heights",
+        "Mount Davidson Manor",
+        "Nob Hill",
+        "Noe Valley",
+        "North Beach",
+        "North Panhandle",
+        "North Waterfront",
+        "Oceanview",
+        "Outer Mission",
+        "Outer Parkside",
+        "Outer Richmond",
+        "Outer Sunset",
+        "Pacific Heights",
+        "Parkside",
+        "Pine Lake Park",
+        "Portola",
+        "Potrero Hill",
+        "Presidio Heights",
+        "Russian Hill",
+        "Sea Cliff",
+        "Sherwood Forest",
+        "Silver Terrace",
+        "South Beach",
+        "South of Market",
+        "St. Francis Wood",
+        "Stonestown",
+        "Sunnyside",
+        "Telegraph Hill",
+        "Tenderloin",
+        "Treasure Isl./Yerba Buena Isl.",
+        "Twin Peaks",
+        "Union Square",
+        "Van Ness/ Civic Center",
+        "Visitacion Valley",
+        "West Portal",
+        "Western Addition",
+        "Westwood Highlands",
+        "Westwood Park",
+        "Yerba Buena"
+    ];
 
-        if (!sfDataResults.ok) {
-            return new Response(
-                JSON.stringify({ error: "data.sfgov.org API error: " + sfDataResults.status }),
-                {
-                    status: 500,
-                    headers: { "Content-Type": "application/json" },
-                }
-            );
-        }
-        const data = await sfDataResults.json();
-        const neighborhoods = data.map(item => item.neighborhood);
-        neighborhoods.sort((a, b) => a.localeCompare(b)); 
-
-        return new Response(JSON.stringify(neighborhoods), {
-            headers: { "Content-Type": "application/json" },
-        });
-    } catch (error) {
-        return new Response(
-            JSON.stringify({ error: "data.sfgov.org server error: " + error.message }),
-            {
-                status: 500,
-                headers: { "Content-Type": "application/json" },
-            }
-        );
-    }
+    return new Response(JSON.stringify(neighborhoods), {
+        headers: { "Content-Type": "application/json" },
+    });
 }
